5d766f324494f669db042b1f213a9821c5ed2f69
Zheng Kou <roddykou@gmail.com>
2013-05-22 17:21:39 -0300

    Update Remove Duplicates from Sorted List II - Garbage.cpp

--- Remove Duplicates from Sorted List II - Garbage.cpp
+++ Remove Duplicates from Sorted List II - Garbage.cpp
@@ -5,17 +5,24 @@ ListNode *deleteDuplicates(ListNode *head) {
         return head;
         
     ListNode root (0);
+    /* use to insert the return list at end */
     ListNode *runnerRet = &root;
+    /* use to traverse the original list */
     ListNode *runnerOrg = head;
     
     while (runnerOrg && runnerOrg->next) {
+        /* different adjacent nodes */
         if (runnerOrg->val != runnerOrg->next->val) {
+            /* add it to the return list */
             runnerRet->next = runnerOrg;
+            /* both move next */
             runnerOrg = runnerOrg->next;
             runnerRet = runnerRet->next;
             runnerRet->next = NULL;
         }
+        /* duplicate nodes */
         else {
+            /* make the runnerOrg the next unique node */
             while (runnerOrg->next && runnerOrg->val == runnerOrg->next->val) {
                 runnerOrg = runnerOrg->next;
             }
@@ -23,6 +30,7 @@ ListNode *deleteDuplicates(ListNode *head) {
         }
         
     }
+    /* insert the last node */
     runnerRet->next = runnerOrg;
     return root.next;
 }
